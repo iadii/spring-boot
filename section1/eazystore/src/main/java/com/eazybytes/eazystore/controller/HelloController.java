5e23f4a3ddba12ef2364fbe1ee43f783855b2183
Aditya Mishra <mishraaditya3456@gmail.com>
2025-05-23 14:00:35 +0530

    request methods

--- section1/eazystore/src/main/java/com/eazybytes/eazystore/controller/HelloController.java
+++ section1/eazystore/src/main/java/com/eazybytes/eazystore/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.eazybytes.eazystore.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,15 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello Adii";
+    }
+
+    @PutMapping("/hello")
+    public String sayHelloPut() {
+        return "Hello put method";
+    }
+
+    @PostMapping("/hello")
+    public String sayHelloPost() {
+        return "Hello post method";
     }
 }
