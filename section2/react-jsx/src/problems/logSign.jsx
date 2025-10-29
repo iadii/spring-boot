ec83fe41f93f6687d13ea08b7eba3a616b8f3d92
Aditya Mishra <mishraaditya3456@gmail.com>
2025-10-29 13:09:54 +0530

    updating

--- section2/react-jsx/src/problems/logSign.jsx
+++
@@ -1,8 +0,0 @@
-function Log() {
-    const isLoggedIn = false;
-    const element = <p> {isLoggedIn ? "Welcome back" : "sign up please"} </p>
-
-    return element;
-}
-
-export default Log;
--- section2/react-jsx/src/problems/mathe.jsx
+++
@@ -1,9 +0,0 @@
-const MathsFun = () => {
-    const a = Math.floor(Math.random() * 100 + 1)
-    const b = Math.floor(Math.random() * 100 + 1)
-
-    return <p>Result is {a + b}</p>
-}
-
-
-export default MathsFun
